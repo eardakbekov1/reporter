330ddde53ba37ed172746b39822716b7d462db27
Maksat254 <125027161+Maksat254@users.noreply.github.com>
2023-03-16 20:28:17 +0600

    migrations created

---
+++ app/Http/Controllers/PrioritiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prioriti;
+use Illuminate\Http\Request;
+
+class PrioritiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Prioriti  $prioriti
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Prioriti $prioriti)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Prioriti  $prioriti
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Prioriti $prioriti)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Prioriti  $prioriti
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Prioriti $prioriti)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Prioriti  $prioriti
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Prioriti $prioriti)
+    {
+        //
+    }
+}
---
+++ app/Models/Prioriti.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prioriti extends Model
+{
+    use HasFactory;
+}
